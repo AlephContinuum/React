3d0ba7a67d6c1f27c43d2e410c2d7d8dd6f404b6
AlephContinuum <sparshsamrat@gmail.com>
2026-06-25 16:10:33 +0530

    1st challenge completed

--- first_react/src/main.jsx
+++ first_react/src/main.jsx
@@ -3,15 +3,21 @@ import "./index.css"
 const root = createRoot(document.getElementById("root"))
 
 root.render(
-    <main>
-        <img src="/images.png" width="40px" alt="React logo" />
-        <h1>Fun fact about react!</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was first released in 2013</li>
-            <li>Was first released in 2013</li>
-            <li>Was first released in 2013</li>
-            <li>Was first released in 2013</li>
-        </ul>
-    </main>
+    <TempName />
 )
+
+function TempName() {
+    return (
+    <main>
+    <img src="/images.png" width="40px" alt="React logo" />
+        <h1>Fun fact about react!</h1>
+        <ol>
+            <li>Was first released in 2013</li>
+            <li>Was created by Jordan Walke</li>
+            <li>Hass over 200k stars on github</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps</li>
+        </ol>
+    </main>
+    )
+}
